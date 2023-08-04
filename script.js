04462b21677c9c8422ea2f45af490979742dbc7d
Erlend <57490453+lite-cyber@users.noreply.github.com>
2023-08-04 15:19:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const armorEl = document.getElementById('armor');
+const finalArmorEl = document.getElementById('finalArmor');
+const mitigationEl = document.getElementById('mitigation');
+const arpEl = document.getElementById('arp');
+const majorDebuffEl = document.getElementById('majorDebuff');
+const minorDebuffEl = document.getElementById('minorDebuff');
+const shatteringDebuffEl = document.getElementById('shatteringDebuff');
+const arpPercentEl = document.getElementById('arpPercent');
+
+const bossArmor = 10643;
+const reduceableCalcConst = 15232.5;
+const arpRating = 13.99;
+
+const calculate = () => {
+  let arp = parseFloat(arpEl.value);
+  let majorDebuff = majorDebuffEl.checked ? 0.20 : 0;
+  let minorDebuff = minorDebuffEl.checked ? 0.05 : 0;
+  let shatteringDebuff = shatteringDebuffEl.checked ? 0.20 : 0;
+
+  let armorAfterDebuff = bossArmor * (1 - majorDebuff) * (1 - minorDebuff) * (1 - shatteringDebuff);
+  let reduceableArmor = Math.min(armorAfterDebuff, (armorAfterDebuff + reduceableCalcConst) / 3);
+  let armorPenetrated = arp ? reduceableArmor * (arp / arpRating / 100) : 0;
+
+  let finalArmor = Math.max(0, armorAfterDebuff - armorPenetrated);
+  let mitigation = finalArmor / (finalArmor + reduceableCalcConst) * 100;
+
+  let totalArmorPenetrated = bossArmor - finalArmor;
+  let arpPercent = totalArmorPenetrated / bossArmor * 100;
+
+  armorEl.innerText = armorAfterDebuff.toFixed(2);
+  finalArmorEl.innerText = finalArmor.toFixed(2);
+  mitigationEl.innerText = mitigation.toFixed(2);
+  arpPercentEl.innerText = arpPercent.toFixed(2);
+};
+
+arpEl.addEventListener('input', calculate);
+majorDebuffEl.addEventListener('input', calculate);
+minorDebuffEl.addEventListener('input', calculate);
+shatteringDebuffEl.addEventListener('input', calculate);
+
+calculate();
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening on port ${port}`);
+});
